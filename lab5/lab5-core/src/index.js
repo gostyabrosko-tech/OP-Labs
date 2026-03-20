4ff6efb5de45728dcada8276fe9072076adf4d88
ArthurKuzmenko <labkiop228@gmail.com>
2026-03-20 10:00:00 +0200

    init lab5 and add callback version

---
+++ lab5/lab5-core/src/index.js
@@ -0,0 +1,46 @@
+export class AbortError extends Error {
+    constructor() {
+        super('The operation was aborted');
+        this.name = 'AbortError';
+    }
+}
+export function mapAsyncCallback(array, fn, callback, signal) {
+    let results = [];
+    let completed = 0;
+    let isDone = false;
+    const timers = new Set();
+    const cleanup = () => {
+        isDone = true;
+        timers.forEach(clearTimeout);
+        timers.clear();
+        if (signal) signal.removeEventListener('abort', onAbort);
+    };
+    const onAbort = () => {
+        cleanup();
+        callback(new AbortError());
+    };
+    if (signal) {
+        if (signal.aborted) return onAbort();
+        signal.addEventListener('abort', onAbort, { once: true });
+    }
+    array.forEach((item, index) => {
+        const timer = setTimeout(() => {
+            timers.delete(timer);
+            if (isDone) return;
+            fn(item, (err, result) => {
+                if (isDone) return;
+                if (err) {
+                    cleanup();
+                    return callback(err);
+                }
+                results[index] = result;
+                completed++;
+                if (completed === array.length) {
+                    cleanup();
+                    callback(null, results);
+                }
+            });
+        }, 100);
+        timers.add(timer);
+    });
+}
