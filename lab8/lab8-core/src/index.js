e35e7f1ac4a591ace95af9ebeb13d8690ccbf730
ArthurKuzmenko <labkiop228@gmail.com>
2026-04-07 12:30:00 +0300

    implement API key and JWT strategies for AuthProxy

--- lab8/lab8-core/src/index.js
+++ lab8/lab8-core/src/index.js
@@ -1,17 +1,35 @@
 export class BaseHttpClient {
     async request(url, options = {}) {
         console.log(`[BaseClient] Fetching: ${url}`);
-        return { 
-            status: 200, 
-            data: { message: "Success", target: url } 
-        };
+        return { status: 200, data: { message: "Success", target: url } };
     }
 }
 export class AuthProxy {
     constructor(client) {
         this.client = client;
+        this.strategy = null;
+    }
+    setStrategy(strategy) {
+        this.strategy = strategy;
     }
     async request(url, options = {}) {
-        return this.client.request(url, options);
+        const finalOptions = this.strategy 
+            ? this.strategy.apply(options) 
+            : options;
+        return this.client.request(url, finalOptions);
     }
 }
+export const AuthStrategies = {
+    apiKey: (key) => ({
+        apply: (opt) => ({
+            ...opt,
+            headers: { ...opt.headers, 'X-API-KEY': key }
+        })
+    }),
+    jwt: (token) => ({
+        apply: (opt) => ({
+            ...opt,
+            headers: { ...opt.headers, 'Authorization': `bearer ${token}` }
+        })
+    })
+};
